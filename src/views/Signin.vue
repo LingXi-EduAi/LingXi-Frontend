9fe6b2cedada09ed375c9aefc1d2b6d9777c1d41
HexWarrior6 <3083512469@qq.com>
2025-02-18 03:15:57 +0800

    first admit

---
+++ src/views/Signin.vue
@@ -0,0 +1,126 @@
+<script setup>
+import { onBeforeUnmount, onBeforeMount } from "vue";
+import { useStore } from "vuex";
+import Navbar from "@/examples/PageLayout/Navbar.vue";
+import ArgonInput from "@/components/ArgonInput.vue";
+import ArgonSwitch from "@/components/ArgonSwitch.vue";
+import ArgonButton from "@/components/ArgonButton.vue";
+const body = document.getElementsByTagName("body")[0];
+
+const store = useStore();
+onBeforeMount(() => {
+  store.state.hideConfigButton = true;
+  store.state.showNavbar = false;
+  store.state.showSidenav = false;
+  store.state.showFooter = false;
+  body.classList.remove("bg-gray-100");
+});
+onBeforeUnmount(() => {
+  store.state.hideConfigButton = false;
+  store.state.showNavbar = true;
+  store.state.showSidenav = true;
+  store.state.showFooter = true;
+  body.classList.add("bg-gray-100");
+});
+</script>
+<template>
+  <div class="container top-0 position-sticky z-index-sticky">
+    <div class="row">
+      <div class="col-12">
+        <navbar
+          isBlur="blur  border-radius-lg my-3 py-2 start-0 end-0 mx-4 shadow"
+          v-bind:darkMode="true"
+          isBtn="bg-gradient-success"
+        />
+      </div>
+    </div>
+  </div>
+  <main class="mt-0 main-content">
+    <section>
+      <div class="page-header min-vh-100">
+        <div class="container">
+          <div class="row">
+            <div
+              class="mx-auto col-xl-4 col-lg-5 col-md-7 d-flex flex-column mx-lg-0"
+            >
+              <div class="card card-plain">
+                <div class="pb-0 card-header text-start">
+                  <h4 class="font-weight-bolder">登录</h4>
+                  <p class="mb-0">输入你的邮箱和密码来登录</p>
+                </div>
+                <div class="card-body">
+                  <form role="form">
+                    <div class="mb-3">
+                      <argon-input
+                        id="email"
+                        type="email"
+                        placeholder="邮件地址"
+                        name="email"
+                        size="lg"
+                      />
+                    </div>
+                    <div class="mb-3">
+                      <argon-input
+                        id="password"
+                        type="password"
+                        placeholder="密码"
+                        name="password"
+                        size="lg"
+                      />
+                    </div>
+                    <argon-switch id="rememberMe" name="remember-me"
+                      >保持登录状态</argon-switch
+                    >
+
+                    <div class="text-center">
+                      <argon-button
+                        class="mt-4"
+                        variant="gradient"
+                        color="success"
+                        fullWidth
+                        size="lg"
+                        >登录</argon-button
+                      >
+                    </div>
+                  </form>
+                </div>
+                <div class="px-1 pt-0 text-center card-footer px-lg-2">
+                  <p class="mx-auto mb-4 text-sm">
+                    还没有账户？
+                    <a
+                      href="javascript:;"
+                      class="text-success text-gradient font-weight-bold"
+                      >注册</a
+                    >
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div
+              class="top-0 my-auto text-center col-6 d-lg-flex d-none h-100 pe-0 position-absolute end-0 justify-content-center flex-column"
+            >
+              <div
+                class="position-relative bg-gradient-primary h-100 m-3 px-7 border-radius-lg d-flex flex-column justify-content-center overflow-hidden"
+                style="
+                  background-image: url(&quot;https://raw.githubusercontent.com/creativetimofficial/public-assets/master/argon-dashboard-pro/assets/img/signin-ill.jpg&quot;);
+                  background-size: cover;
+                "
+              >
+                <span class="mask bg-gradient-success opacity-6"></span>
+                <h2
+                  class="mt-5 text-white font-weight-bolder position-relative"
+                >
+                  "AI赋能教育，点亮智慧未来"
+                </h2>
+                <hr>
+                <p class="text-white position-relative">
+                  智能教学 · 个性化学习 · 数据驱动教育创新
+                </p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </main>
+</template>
