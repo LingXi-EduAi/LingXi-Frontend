6f6dcb118ca09724f2e970232337542eb0faefe4
HexWarrior6 <3083512469@qq.com>
2025-03-02 01:23:24 +0800

    更改了页脚信息

--- src/examples/Footer.vue
+++ src/examples/Footer.vue
@@ -18,9 +18,8 @@ const store = useStore();
               href="https://www.creative-tim.com"
               class="font-weight-bold"
               target="_blank"
-              >Creative Tim</a
+              >LingXi EduAI</a
             >
-            for a better web.
           </div>
         </div>
         <div class="col-lg-6">
@@ -29,34 +28,18 @@ const store = useStore();
           >
             <li class="nav-item">
               <a
-                href="https://www.creative-tim.com"
+                href="https://gitee.com/HexWarrior6/ling-xi-edu-ai-ui"
                 class="nav-link text-muted"
                 target="_blank"
-                >Creative Tim</a
+                >LingXi EduAI</a
               >
             </li>
             <li class="nav-item">
               <a
-                href="https://www.creative-tim.com/presentation"
+                href="https://gitee.com/HexWarrior6/ling-xi-edu-ai-ui"
                 class="nav-link text-muted"
                 target="_blank"
                 >About Us</a
-              >
-            </li>
-            <li class="nav-item">
-              <a
-                href="https://www.creative-tim.com/blog"
-                class="nav-link text-muted"
-                target="_blank"
-                >Blog</a
-              >
-            </li>
-            <li class="nav-item">
-              <a
-                href="https://www.creative-tim.com/license"
-                class="nav-link pe-0 text-muted"
-                target="_blank"
-                >License</a
               >
             </li>
           </ul>
