361831ee1be59e7770f2bdd41b724127faa87677
zhekaiMao <m13736130402@gmail.com>
2025-07-10 01:23:31 +0800

    feat(路由): 添加学生个人设置路由并更新侧边栏链接
    将个人设置页面添加到学生路由下，并更新侧边栏导航链接指向新路由路径

--- src/examples/Sidenav/StudentSidenavList.vue
+++ src/examples/Sidenav/StudentSidenavList.vue
@@ -144,7 +144,7 @@ const getRoute = () => {
       </li>
 
       <li class="nav-item">
-        <sidenav-item to="/profile" :class="getRoute() === 'profile' ? 'active' : ''"
+        <sidenav-item to="/student/profile" :class="getRoute() === 'profile' ? 'active' : ''"
           :navText="isRTL ? 'حساب تعريفي' : '个人设置'">
           <template v-slot:icon>
             <svg width="24" height="24" viewBox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg"
